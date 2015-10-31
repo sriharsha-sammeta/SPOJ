a418c0e72881f74362e4091ce89d710f239621ec
sriharsha4444 <sriharsha4444@icloud.com>
2015-11-01 01:05:45 +0530

    powershell script to replace VS solution with cpp file only

---
+++ cleanup.ps1
@@ -0,0 +1,9 @@
+﻿if(Test-path $args\$args\Source.cpp){
+    echo "$args.cpp File exists\n"
+    echo "moving it ...\n"    
+    Copy $args\$args\Source.cpp .\$args.cpp
+    echo "deleting the $args directory...\n"
+    Remove-Item .\$args -Recurse -Force
+    }else{
+echo "$args\$args\Source.cpp not found"
+}
